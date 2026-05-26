4b7907e4028678dd7225624e9d6b67778c8ceac0
gojo-2405 <anonymoushacker24110509@gmail.com>
2026-05-26 15:24:32 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+import boto3
+import json
+import os
+
+app = Flask(__name__)
+
+sqs = boto3.client(
+    "sqs",
+    region_name="us-east-1"
+)
+
+QUEUE_URL = os.environ.get("QUEUE_URL")
+
+@app.route("/order", methods=["POST"])
+def create_order():
+
+    data = request.json
+
+    message = {
+        "type": data.get("type"),
+        "order_id": data.get("order_id"),
+        "amount": data.get("amount"),
+        "user": data.get("user")
+    }
+
+    sqs.send_message(
+        QueueUrl=QUEUE_URL,
+        MessageBody=json.dumps(message)
+    )
+
+    return jsonify({
+        "message": "Message sent to SQS",
+        "data": message
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
